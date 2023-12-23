21790014a641285da59d355ec6103a0a6694b698
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-23 20:36:22 -0300

    Users table no formato original
    Deixar a users_table no formato original pois não havia necessidade de mexer na mesma

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('cpf');
-            $table->string('date_birth');
-            $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
