2fe9b6ec0a22b5e920a170bed2ad2fe82dd86c60
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-23 20:37:32 -0300

    Atualização da User Controller
    Atualização da User Controller depois dos testes rodados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    public function index(){
-        return "Ai Brasiiiiiiiiil";
-    }
+    use HttpResponses;
 
     public function store(Request $request){
-        var_dump($request->input('name'));
+
+        try{
+
+            $data = $request->all();
+
+            $request->validate([
+                'name'=>'string|required|max:255',
+                'email'=>'string|required|unique:users|max:255',
+                'date_birth'=>'string|required',
+                'cpf'=> 'string|required|unique:users|max:14',
+                'password'=>'string|required|min:8|max:32',
+                'plan_id' => 'required'
+            ]);
+
+            $user = User::create($data);
+
+            return $user;
+
+        } catch(\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
     }
+
 }
