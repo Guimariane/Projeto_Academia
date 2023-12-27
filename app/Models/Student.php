1d8f71d8fe7347b72ac46239cd8d68022784047f
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-27 15:41:55 -0300

    Criação do Model
    Criação do Model Student com os campos que devem ser preenchidos

---
+++ app/Models/Student.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $table = 'students';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'date_birth',
+        'cpf',
+        'contact',
+        'cep',
+        'street',
+        'state',
+        'neighborhood',
+        'city',
+        'number'];
+
+}
