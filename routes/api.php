c67c39639f03e1899f204eeef5370383360d951f
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-28 03:34:05 -0300

    Adição de Rotas
    Divisão entre rotas públicas (login e users) e adição de rotas privadas (logout e students)

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,20 @@
 <?php
 
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    // rotas privadas
-});
+/* Rotas públicas */
+Route::post('login', [AuthController::class, 'store']);
+Route::post('users', [UserController::class, 'store']);
 
-// rota pública
+
+/* Rotas privadas */
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::post('students', [StudentController::class, 'store']);
+    Route::get('students', [StudentController::class, 'index']);
+
+});
