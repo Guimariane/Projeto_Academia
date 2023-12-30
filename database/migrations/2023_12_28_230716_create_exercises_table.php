8ceff308a554813af7191d8bdb96f8ca2ba2b055
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-30 03:22:12 -0300

    Exclusão de coluna
    Exclusão da coluna contact que foi colocada erroneamente na migração

--- database/migrations/2023_12_28_230716_create_exercises_table.php
+++ database/migrations/2023_12_28_230716_create_exercises_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('exercises', function (Blueprint $table) {
             $table->id();
             $table->string('description', 255);
-            $table->string('contact', 20);
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
