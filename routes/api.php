e4b49d0abb72e10dadab60d61bafe6f05816911c
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-30 01:40:42 -0300

    Adição de rotas privadas
    Adição das rotas privadas de exercícios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -14,11 +15,16 @@ Route::post('users', [UserController::class, 'store']);
 /* Rotas privadas */
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
+
     Route::get('students', [StudentController::class, 'index']);
     Route::post('students', [StudentController::class, 'store']);
     Route::put('students/{id}', [StudentController::class, 'update']);
     Route::delete('students/{id}', [StudentController::class, 'destroy']);
     Route::get('students/{id}', [StudentController::class, 'show']);
+
+    Route::get('exercises', [ExerciseController::class, 'index']);
+    Route::post('exercises', [ExerciseController::class, 'store']);
+    Route::delete('exercises', [ExerciseController::class, 'destroy']);
 
 
 });
