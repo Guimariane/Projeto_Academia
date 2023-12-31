5bd2bce9c03a30ba02acd23ed3d8b946da988bb3
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-12-31 03:49:46 -0300

    Importações e Edição de Funções
    importação do módulo de PDF para a exportação no treino do estudante, ajuste na função de store e adição da função de index e export

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -2,19 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use App\Models\Workout;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class WorkoutController extends Controller
 {
+    // Função para cadastro de treinos
     public function store(Request $request){
         try {
             $data = $request->validate([
+                'student_id' => 'int|required',
+                'exercise_id' => 'int|required',
                 'repetitions' => 'int|required',
                 'weight' => 'numeric|required',
                 'break_time' => 'int|required',
-                'day' => 'required|in: SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO',
+                'day' => 'required|in:SEGUNDA,TERCA,QUARTA,QUINTA,SEXTA,SABADO,DOMINGO',
                 'observations' => 'string',
                 'time' => 'int|required'
 
@@ -26,11 +31,15 @@ class WorkoutController extends Controller
                 return response()->json(['message' => 'Usuario nao autenticado'], Response::HTTP_BAD_REQUEST);
             }
 
-            $search = $request->input('exercise_id');
+            $student = $request->input('student_id');
+
+            $exercise = $request->input('exercise_id');
 
             $day = $request->input('day');
 
-            $data_workouts = Workout::query()->where(['exercise_id', $search])->count();
+            $data_workouts = Workout::query()
+                    ->where(['student_id' => $student, 'exercise_id' => $exercise, 'day' => $day])
+                    ->count();
 
             if($data_workouts !== 0) return $this->error("Você já cadastrou esse exercício nesse dia", Response::HTTP_CONFLICT);
 
@@ -45,4 +54,94 @@ class WorkoutController extends Controller
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
-}
+
+    // Função para listagem de treinos
+    public function index(Request $request, $id){
+        $user = $request->user();
+
+        $students = $user->students()->findOrFail($id);
+
+        $student_name = $students->name;
+
+        $workouts = Workout::where('student_id', $id)->get();
+
+        $response = [
+            'student_id' => $id,
+            'student_name' => $student_name,
+            'workouts' => [
+                'SEGUNDA' => [],
+                'TERCA' => [],
+                'QUARTA' => [],
+                'QUINTA' => [],
+                'SEXTA' => [],
+                'SABADO' => [],
+                'DOMINGO' => [],
+            ],
+        ];
+
+        foreach ($workouts as $workout) {
+            $day = strtoupper($workout->day);
+            $response['workouts'][$day][] = [
+                'description' => $workout->exercises->description,
+                'repetitions' => $workout->repetitions,
+                'weight' => $workout->weight,
+                'break_time' => $workout->break_time,
+                'day' => $workout->day,
+                'observations' => $workout->observations,
+                'time' => $workout->time,
+            ];
+        }
+
+        // apenas criar o pdf e passar a variável $students, $workouts e $response
+
+        return response()->json($response, 200);
+    }
+
+    // Função para criar o pdf
+    public function export (Request $request, $id){
+
+        $user = $request->user();
+
+        $students = $user->students()->findOrFail($id);
+
+        $student_name = $students->name;
+
+        $workouts = Workout::where('student_id', $id)->get();
+
+        $response = [
+            'student_id' => $id,
+            'student_name' => $student_name,
+            'workouts' => [
+                'SEGUNDA' => [],
+                'TERCA' => [],
+                'QUARTA' => [],
+                'QUINTA' => [],
+                'SEXTA' => [],
+                'SABADO' => [],
+                'DOMINGO' => [],
+            ],
+        ];
+
+        foreach ($workouts as $workout) {
+            $day = strtoupper($workout->day);
+            $response['workouts'][$day][] = [
+                'description' => $workout->exercises->description,
+                'repetitions' => $workout->repetitions,
+                'weight' => $workout->weight,
+                'break_time' => $workout->break_time,
+                'day' => $workout->day,
+                'observations' => $workout->observations,
+                'time' => $workout->time,
+            ];
+        }
+
+        $pdf = Pdf::loadView('pdf.treino', [
+            'students' => $students,
+            'workouts' => $workouts,
+            'response' => $response
+        ]);
+
+        return $pdf->stream('treino.pdf');
+
+    }
+    }
